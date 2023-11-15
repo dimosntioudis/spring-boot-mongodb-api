39446aee9de6bf95778d4017c497d38fb00c5cb7
Dimos Nt <dimos.ntioudis@gmail.com>
2023-11-15 12:00:57 +0200

    Update AuthController.java

--- src/main/java/com/certh/spring/data/mongodb/controller/AuthController.java
+++ src/main/java/com/certh/spring/data/mongodb/controller/AuthController.java
@@ -29,13 +29,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:63342", maxAge = 3600, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
