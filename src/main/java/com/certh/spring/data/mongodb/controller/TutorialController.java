34e3fc4eae7b46be1348f14e71622c3480fa6bb8
Dimos Nt <dimos.ntioudis@gmail.com>
2023-11-15 12:01:31 +0200

    Removed cros configuration from controllers

--- src/main/java/com/certh/spring/data/mongodb/controller/TutorialController.java
+++ src/main/java/com/certh/spring/data/mongodb/controller/TutorialController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.certh.spring.data.mongodb.models.Tutorial;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
